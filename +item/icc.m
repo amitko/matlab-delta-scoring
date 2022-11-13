f1a115d633fe04f95ee7e996ec3de0bc6ffdbfab
Dimitar Atanasov <datanasov@ir-statistics.net>
2022-11-13 10:48:51 +0300

    delta_api

--- +item/icc.m
+++ +item/icc.m
@@ -1,18 +1,18 @@
-function res = icc(itemParameters,o)
+function res = icc(itemParameters,o,legendStr)
 % res = icc(itemParameters,o)
 % Plots ICC curves under given item parameters
 %
-% INPUT: 
+% INPUT:
 %	itemParameters
 %	o - options
 %
-% OUTPUT: 
+% OUTPUT:
 %	res  - figure handle
 
 % Dimitar Atanasov, 2017
 % datanasov@ir-statistics.net
 
-if nargin < 2
+if nargin < 2 || isempty(o)
     o = deltaScoring.scoring.Options;
 end
 
@@ -29,10 +29,21 @@ x = linspace(0,1);
 res = figure;
 hold on;
 
+cl = 1;
+tp = 1;
 for k = 1:size(itemParameters,1)
-    
+
     pcr = deltaScoring.scoring.PCR(itemParameters(k,:),x,o);
-    plot(x,pcr,'k-', 'LineWidth',1.5);
+    if  isfield(o,'colorPlot') && o.colorPlot == 1
+        plot(x,pcr,[c(cl) t(tp) '-.'], 'LineWidth',1.5);
+        cl = cl + 1;
+        if cl == 8
+            tp = tp + 1;
+            cl = 1;
+        end
+    else
+        plot(x,pcr,'k-', 'LineWidth',1.5);
+    end
 end
 
 xlim([0 1]);
@@ -40,4 +51,7 @@ ylim([0 1]);
 xlabel('D-score');
 ylabel('Probability for correct response');
 
+if nargin == 3 && ~isempty(legendStr)
+    legend(legendStr);
+end
 hold off;
