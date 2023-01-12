a282f070134b1d6b3ece74783d6acccab3706b44
Dimitar <datanasov@ir-statistics.net>
2023-01-12 11:19:49 +0200

    lsdmAttributes4cd-fix

--- +diagnostics/lsdmAttributes4cd.m
+++ +diagnostics/lsdmAttributes4cd.m
@@ -1,4 +1,4 @@
-function [res at_least] = lsdmAattributes4cd(attr_performance)
+function [res at_least] = lsdmAttributes4cd(attr_performance)
 % Function lsdmAttributes4cd(attr_performance)
 %
 % Calculates the probabilities for performing on k
@@ -19,28 +19,28 @@ function [res at_least] = lsdmAattributes4cd(attr_performance)
 
 res = [];
 
-for t = 1 : size( attr_performance,2 ) % for each ability level
+for t = 1 : size( attr_performance,1 ) % for each ability level
     r = [];
-    s = -sort(-attr_performance(:,t));
-    for p = 0:size( attr_performance,1 ) % for each  'p'
-        r = [r comb_probability( attr_performance(:,t)',p) ];
+    s = -sort(-attr_performance(t,:));
+    for p = 0:size( attr_performance,2 ) % for each  'p'
+        r = [r comb_probability( attr_performance(t,:),p) ];
     end
-    res = [res r'];
+    res = [res; r];
 end
 
 resc = zeros(size(res));
 
 resc(end,:) = res(end,:);
 
-for t = size(res,1)-1:-1:1
-    resc(t,:) = resc(t + 1,:) + res( t ,:);
+for t = size(res,2)-1:-1:1
+    resc(:,t) = resc(:,t + 1) + res(:, t);
 end
 
 
 at_least = [];
-for k = 1 : size( attr_performance,2 )
-    for l = 1:size(res,1)
-        at_least(l,k) = sum(res(l:end,k));
+for k = 1 : size( attr_performance,1 )
+    for l = 1:size(res,2)
+        at_least(k,l) = sum(res(k,l:end));
     end
 end
 
